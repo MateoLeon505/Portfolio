3ead2e1a544468640bee20d0a0c891d8d724d363
mateoleont <mateolt505@gmail.com>
2023-12-31 00:19:41 -0500

    Comp

--- src/Components/FrontPage/frontPage.jsx
+++ src/Components/FrontPage/frontPage.jsx
@@ -8,14 +8,15 @@ const FrontPage = () => {
         container
         direction='row'
         className={styles.gridContainer}>
-            <Grid item xs>
+            <Grid item xs={4}>
                 <Typography variant='h2' align='center' gutterBottom > Hi everyone 👋 </Typography>
                 <Typography variant='body1' align='center' ><b> Mi nombre es Mateo León </b></Typography>
-                <Typography variant='h5' align='center'>Soy:</Typography>
+                <Typography variant='h6' align='center'>Soy:</Typography>
                 <Typography variant='body1' align='center'>Fullstack devoloper</Typography>
                 <Typography variant='body1' align='center'>Frontend devoloper</Typography>
+                <Typography variant='body1' align='center'>Estudiante de Ingeniería Multimedia</Typography>
             </Grid>
-            <Grid item xs>
+            <Grid item xs={6}>
                 <img src="https://c0.wallpaperflare.com/preview/856/202/494/black-laptop.jpg" alt="desktop"  className={styles.image}/>
             </Grid>
         </Grid>
--- src/Components/description/text.jsx
+++ src/Components/description/text.jsx
@@ -9,18 +9,18 @@ const Text = () => {
             container
             className={styles.gridContainer}
             spacing={2}>
-                <Grid item width='65%'>
+                <Grid item width='80%'>
                     <Typography variant='body1' align='center' gutterBottom >
                         Soy estudiante de ingeniería multimedia y desarrollador web fullstack.
                     </Typography>
                 </Grid>
-                <Grid item width='65%'>
+                <Grid item width='80%'>
                     <Typography variant='body1' align='center' gutterBottom>
                         <b>Mi enfoque está en el desarrollo frontend, donde trabajo para crear soluciones web 
                         elegantes y funcionales que brinden una experiencia única a los usuarios.</b>
                     </Typography>
                 </Grid>
-                <Grid item width='65%'>
+                <Grid item width='80%'>
                     <Typography variant='body1' align='center'>
                         Mis objetivos profesionales son crecer como desarrollador web, explorar el emocionante 
                         mundo del desarrollo de aplicaciones móviles y contribuir a proyectos que generen un 
