0b7bdd7c12bb94772609ae98e285d03650698889
mateoleont <mateolt505@gmail.com>
2024-01-08 13:51:01 -0500

    creacion banner

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -7,16 +7,6 @@ const Home = () => {
 
     return(
         <div>
-            {/* <Grid 
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            marginTop='4%'
-            marginBottom='4%'>
-                <Grid item xs='6'></Grid>
-                <Grid item xs='6'>Hola</Grid>
-            </Grid> */}
             <FrontPage/>
             <Text/>
         </div>
