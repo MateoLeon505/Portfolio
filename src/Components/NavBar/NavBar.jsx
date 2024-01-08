f7a0813f2d10f74a18566ac6a210eab821049d47
mateoleont <mateolt505@gmail.com>
2024-01-08 13:46:35 -0500

    navbar listo

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -35,47 +35,49 @@ const NavBar = ()  => {
   }
 
   return (
-
     <Navbar expand="lg" className={styles.navbar}>
       <Container >
-        <Navbar.Brand href="#home" className={styles.brand}>
-          <img src={logo} alt="Logo" />
-        </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className={styles.navbarTogglerIcon}></span>
-        </Navbar.Toggle> */}
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="#home" className={ activeLink === 'home' ? styles.active_navbarlink : styles.navbarlink }
-            onClick={()=> onUpdateActiveLink('home')}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="#about" className={ activeLink === 'about' ? styles.active_navbarlink : styles.navbarlink }
-            onClick={()=> onUpdateActiveLink('about')}>
-              About
-            </Nav.Link>
-            <Nav.Link href="#skills" className={ activeLink === 'skills' ? styles.active_navbarlink : styles.navbarlink }
-            onClick={()=> onUpdateActiveLink('skills')}>
-              Skills
-            </Nav.Link>
-            <Nav.Link href="#projects" className={ activeLink === 'projects' ? styles.active_navbarlink : styles.navbarlink }
-            onClick={()=> onUpdateActiveLink('projects')}>
-              Proyects
-            </Nav.Link>
-            <Nav.Link href="#contact" className={ activeLink === 'contact' ? styles.active_navbarlink : styles.navbarlink }
-            onClick={()=> onUpdateActiveLink('contact')}>
-              Contact
-            </Nav.Link>
-          </Nav>
-          {/* <span className={styles.navbar_text}>
-            <div className={styles.social_icon}>
-              <a href="#"><img src={navIcon1} alt="Linkedin" /></a>
-              <a href="#"><img src={navIcon2} alt="Github" /></a>
-              <a href="#"><img src={navIcon3} alt="StackOverflow" /></a>
-              <button className={styles.connect} onClick={()=>alert('connect') }><span>Let´s connect</span></button>
-            </div>
-          </span> */}
-        </Navbar.Collapse>
+        <div className={styles.navigation}>
+          <div className={styles.brand}>
+            <Navbar.Brand href="#home">
+              <img src={logo} alt="Logo" className={styles.logo}/>
+            </Navbar.Brand>
+          </div>
+          <div className={styles.items}>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link href="#home" className={ activeLink === 'home' ? styles.active_navbarlink : styles.navbarlink }
+                onClick={()=> onUpdateActiveLink('home')}>
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#about" className={ activeLink === 'about' ? styles.active_navbarlink : styles.navbarlink }
+                onClick={()=> onUpdateActiveLink('about')}>
+                  About
+                </Nav.Link>
+                <Nav.Link href="#skills" className={ activeLink === 'skills' ? styles.active_navbarlink : styles.navbarlink }
+                onClick={()=> onUpdateActiveLink('skills')}>
+                  Skills
+                </Nav.Link>
+                <Nav.Link href="#projects" className={ activeLink === 'projects' ? styles.active_navbarlink : styles.navbarlink }
+                onClick={()=> onUpdateActiveLink('projects')}>
+                  Proyects
+                </Nav.Link>
+                <Nav.Link href="#contact" className={ activeLink === 'contact' ? styles.active_navbarlink : styles.navbarlink }
+                onClick={()=> onUpdateActiveLink('contact')}>
+                  Contact
+                </Nav.Link>
+              </Nav>
+              <span className={styles.navbar_text}>
+                {/* <div className={styles.social_icon}>
+                  <a href="#"><img src={navIcon1} alt="Linkedin" /></a>
+                  <a href="#"><img src={navIcon2} alt="Github" /></a>
+                  <a href="#"><img src={navIcon3} alt="StackOverflow" /></a>
+                  <button className={styles.connect} onClick={()=>alert('connect') }><span>Let´s connect</span></button>
+                </div> */}
+              </span>
+            </Navbar.Collapse>
+          </div>
+        </div>
       </Container>
     </Navbar>
   );
