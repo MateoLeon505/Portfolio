35d51ca9aac410cf1124137d37cf1e6cb8d84155
mateoleont <mateolt505@gmail.com>
2024-01-08 22:58:11 -0500

    banner en proceso

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { ArrowRightCircle } from 'react-bootstrap-icons'
 import styles from './Banner.module.css';
+import header from '../../assets/img/header-img.svg';
 
 const Banner = () => {
 
+    const toRotate = ['Desarollador Frontend', 'Desarollador FullStack', 'Estudiante de ingeniería multimedia'];
+    const [ loopNum, setLoopNum ] = useState(0);
+    const [ isDeleting, setIsDeleting ] = useState(false);
+    const [ text, setText ] = useState('');
+    const [ delta, setDelta ] = useState(Math.random() * 100)
+    const period = 2000;
+
+    useEffect (() => {
+
+        let ticker = setInterval(() => {
+          tick();
+        }, delta)
+
+        return () => { clearInterval(ticker) }
+
+    }, [text]);
+    
+    const tick = () => {
+
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+        
+        setText(updateText);
+    }
+
     return (
-        <div className={styles.bannerContainer}>
+        //<div className={styles.bannerContainer}>
             <section className={styles.Banner} id='home'>
                 <Container>
                     <Row className={styles.row}>
@@ -14,17 +42,20 @@ const Banner = () => {
                                 <h1>Bienvenido a mi portfolio 👋</h1>
                             </span>
                             <h1>Soy</h1>
-                            <h4>Desarollador Frontend</h4>
-                            <h4>Desarollador FullStack</h4>
-                            <h4>Estudiante de ingeniería multimedia</h4>
-                            <button className={styles.connect} onClick={()=>alert('connect') }><ArrowRightCircle/> <span>Let´s connect</span></button>
+                            <h4>{toRotate.map((option) => 
+                             <h></h>   
+                            )}</h4>
+                            <button className={styles.connect} onClick={()=>alert('connect') }>
+                                <span>Let´s connect <ArrowRightCircle size={25}/></span>
+                            </button>
                         </Col>
-                        <Col></Col>
-                        
+                        <Col xs='12' md='6' xl='5'>
+                            <img src={header} alt="HeaderImage" />
+                        </Col>
                     </Row>
                 </Container>
             </section>
-        </div>
+        //</div>
     );
 }
 
