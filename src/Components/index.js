78584546edff65af6d24083ccb293ffc4d774cac
mateoleont <mateolt505@gmail.com>
2024-01-09 17:13:39 -0500

    cambios medianos

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,5 @@
 import NavBar from "./NavBar/NavBar";
 import Text from "./description/text";
-import FrontPage from "./FrontPage/frontPage";
 import Banner from "./Banner/Banner";
 
-export { NavBar, Text, FrontPage, Banner };
+export { NavBar, Text, Banner };
