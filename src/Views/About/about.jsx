68a9c630b3153efbe2ccde44809b8cdaf8628721
mateoleont <mateolt505@gmail.com>
2024-01-23 09:54:59 -0500

    changes

--- src/Views/About/about.jsx
+++ src/Views/About/about.jsx
@@ -1,16 +1,42 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 import styles from  './about.module.css';
-import reactImage from '../../assets/img/skills/react.svg';
 
 const About = () => {
 
     return (   
         <section className={styles.container}>
             <Grid
-            container
-            className={styles.gridContainer}
-            columnSpacing={2}>
-                <Grid item width='80%'>
+                container
+                direction='row'
+                className={styles.gridContainer}>
+                <Grid item xs={4} md='' xl=''>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            sx={{ height: 140 }}
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            title="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4} md='' xl=''>
                     <Typography variant='body1' align='center' gutterBottom >
                         Soy estudiante de ingeniería multimedia y desarrollador web fullstack.
                     </Typography>
@@ -22,12 +48,6 @@ const About = () => {
                         Mis objetivos profesionales son crecer como desarrollador web, explorar el emocionante 
                         mundo del desarrollo de aplicaciones móviles y contribuir a proyectos que generen un 
                         impacto positivo en la sociedad.
-                    </Typography>
-                </Grid>
-                <Grid item width='80%'>
-                    <Typography variant='body1' align='center' gutterBottom>
-                        <b>Mi enfoque está en el desarrollo frontend, donde trabajo para crear soluciones web 
-                        elegantes y funcionales que brinden una experiencia única a los usuarios.</b>
                     </Typography>
                 </Grid>
             </Grid>
