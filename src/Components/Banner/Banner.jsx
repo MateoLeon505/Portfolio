ede0a9329b3866c9eeaed9affe50691d09640d4d
mateoleont <mateolt505@gmail.com>
2024-01-23 23:31:45 -0500

    Ajustes Banner Responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,23 +3,22 @@ import { Grid, Typography } from "@mui/material"
 import styles from './Banner.module.css';
 import coding from '../../assets/img/Code typing-bro.svg';
 
-
 const Banner = () => {
 
     return (
         <section className={styles.bannerContainer} id='home'>
             <Grid
                 container
-                direction='row'
+                direction={{ xs: 'column', lg: 'row' }} 
                 className={styles.gridContainer}>
-                <Grid item xs={4} md='' xl=''>
+                <Grid item xs={12} lg={6}>
                     <Typography variant='h2' align='center' gutterBottom > Bienvenido a mi portfolio 👋 </Typography>
                     <Typography variant='body1' align='center' ><b> Mi nombre es Mateo León, soy: </b></Typography>
                     <Typography variant='body1' align='center'>Fullstack / Frontend Developer</Typography>
                     <Typography variant='body1' align='center'>&</Typography>
                     <Typography variant='body1' align='center'>Estudiante de Ingeniería Multimedia</Typography>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} lg={6}>
                     <div className={styles.imageContainer}>
                         <img src={coding} alt="HeaderImage" /> 
                     </div>               
