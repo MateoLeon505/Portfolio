107fe9e72f244abab5a8babe673ceb2ab368e352
mateoleont <mateolt505@gmail.com>
2024-01-30 16:39:16 -0500

    listo abaut, services en proceso

--- src/Views/Services/services.jsx
+++ src/Views/Services/services.jsx
@@ -1,9 +1,32 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import styles from './services.module.css';
+
 const Services = () => {
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
     return (
-        <div>
-          <h1>Habilidades</h1>
-        </div>
-      );
+      <Box sx={{ flexGrow: 1 }} className={styles.skillsContainer}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {Array.from(Array(6)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Item>xs=2</Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    );
 }
 
 export default Services;
