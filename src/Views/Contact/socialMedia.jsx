4983f0176702a03b8057985ea42b88d0b68ecdec
mateoleont <mateolt505@gmail.com>
2024-02-10 12:42:27 -0500

    iconos

--- src/Views/Contact/socialMedia.jsx
+++ src/Views/Contact/socialMedia.jsx
@@ -12,7 +12,7 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import styles from './socialMedia.module.css';
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: 'center',
@@ -34,7 +34,7 @@ export default function SocialMedia() {
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {icons.map((item, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
-            <Item>{item.icon}</Item>
+            <div>{item.icon}</div>
           </Grid>
         ))}
       </Grid>
