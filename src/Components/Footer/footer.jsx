eefbb80b35ca80fea254241b5baf44c059112764
mateoleont <mateolt505@gmail.com>
2024-03-04 12:25:43 -0500

    styles foot

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,36 +1,34 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
+import { TiSocialLinkedinCircular } from "react-icons/ti";
 import { FaGithub } from "react-icons/fa";
-import { SiGmail } from "react-icons/si"; 
-import { IoLogoWhatsapp } from "react-icons/io";
 import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerSections}>
+      <div>
+        <Typography variant="body1" align="center">
+          <BsFillInfoCircleFill /> Info +
+        </Typography>
+      </div>
+      <div>
         <div>
-          <Typography variant="body1" align="center" >
-            <BsFillInfoCircleFill/> Info +
-          </Typography>
-        </div>
-        <div>
-          <Typography variant="h6" align="center" >
+          <Typography variant="h6" align="center">
             Mateo León
           </Typography>
         </div>
         <div className={styles.iconsContainer}>
-            <FaLinkedin className={styles.icon}/>
-            <FaGithub className={styles.icon}/>
-            {/* <IoLogoWhatsapp className={styles.icon}/>
-            <SiGmail className={styles.icon}/> */}
+          <FaLinkedin className={styles.icon} />  
+          <FaGithub className={styles.icon} />
+          <TiSocialLinkedinCircular className={styles.icon} />
         </div>
       </div>
       <div className={styles.copyright}>
-        <Typography variant="body2" align="center" >
-          Copyright ©2024 todos los derechos reservados
+        <Typography variant="body2" align="center">
+          Copyright ©2024
         </Typography>
       </div>
     </footer>
