d7ae922e246673330e333f5cc568bc241f61edb3
mateoleont <mateolt505@gmail.com>
2024-03-04 12:18:12 -0500

    last ch

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import styles from './footer.module.css';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si"; 
+import { IoLogoWhatsapp } from "react-icons/io";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <Box >
-            <Typography variant="body2"  align='center' gutterBottom >
-                © Copyright 2024
-            </Typography>
-        </Box>
+      <div className={styles.footerSections}>
+        <div>
+          <Typography variant="body1" align="center" >
+            <BsFillInfoCircleFill/> Info +
+          </Typography>
+        </div>
+        <div>
+          <Typography variant="h6" align="center" >
+            Mateo León
+          </Typography>
+        </div>
+        <div className={styles.iconsContainer}>
+            <FaLinkedin className={styles.icon}/>
+            <FaGithub className={styles.icon}/>
+            {/* <IoLogoWhatsapp className={styles.icon}/>
+            <SiGmail className={styles.icon}/> */}
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <Typography variant="body2" align="center" >
+          Copyright ©2024 todos los derechos reservados
+        </Typography>
+      </div>
     </footer>
   );
 };
